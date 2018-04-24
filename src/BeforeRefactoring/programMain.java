5aa57b06496397e6af6775cbe426d08ecb3bab74
jessio <jessitondsouza@gmail.com>
2018-04-24 13:19:03 +0530

    final commit

--- src/BeforeRefactoring/programMain.java
+++ src/BeforeRefactoring/programMain.java
@@ -14,7 +14,16 @@ public class programMain {
 		has2.addlights();
 		has2.addAC();
 		has2.addTV();
-		has2.displayDeviceList();
+		System.out.println("USER: "+has2.owner.getName() );
+		for(int i=0;i<has2.devices;i++){
+			if(has2.arr[i][1]=="AC")
+				System.out.println("Device: "+((AC)has2.arr[i][0]).name);
+			if(has2.arr[i][1]=="TV")
+				System.out.println("Device: "+((TV)has2.arr[i][0]).name);
+			if(has2.arr[i][1]=="light")
+				System.out.println("Device: "+((light)has2.arr[i][0]).name);
+		}
+		System.out.println("");
 	}
 
 }
